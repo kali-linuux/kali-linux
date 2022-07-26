c9a23e68b47ed8273c3677782ea160fe62af3739
kali-linuux <109958293+kali-linuux@users.noreply.github.com>
2022-07-26 16:18:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -249,7 +249,7 @@ async def account_login(bot: Client, m: Message):
                         
                     else:
                         link="https://www.youtube.com/embed/"+bcvid
-                     await m.reply_text(link)
+                     await m.reply_text(link indent 1)
 
                     editable3= await m.reply_text("**Now send the Resolution**")
                     input4 = message = await bot.listen(editable.chat.id)
