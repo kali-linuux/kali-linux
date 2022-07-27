4cad26af473b4afd066e02279ae6364862e2b940
kali-linuux <109958293+kali-linuux@users.noreply.github.com>
2022-07-27 23:54:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -115,7 +115,10 @@ async def account_login(bot: Client, m: Message):
     #info["password"] = raw_text.split("*")[1]
     #await m.reply_text(raw_text)
     #await input1.delete(True)
-    
+    headers = {
+           'Content-Type': 'application/json',
+           'Accept': 'application/json'
+}
     login_response=requests.post(url+"login-other",info)
     await m.reply_text(login_response.status_code)
     
