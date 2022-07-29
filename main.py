888def9d1ac08d49f0a850a1e20d114472ffd707
kali-linuux <109958293+kali-linuux@users.noreply.github.com>
2022-07-29 09:55:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -84,7 +84,7 @@ async def account_login(bot: Client, m: Message):
     # await input1.delete(True)
 
     login_response = requests.post(url, info)
-    print("printing the response {}",  login_response.read())
+    print("printing the response {}",  login_response)
     token = response_json.json()["data"]["token"]
     await editable.edit("**login Successful**")
     # await editable.edit(f"You have these Batches :-\n{raw_text}")
