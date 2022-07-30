95cf25a648a80e90a918603689e3ef1a036aadd7
kali-linuux <109958293+kali-linuux@users.noreply.github.com>
2022-07-31 00:05:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -39,9 +39,6 @@ from subprocess import getstatusoutput
 import logging
 import os
 import re
-import simplejson
-
-import requests
 
 bot = Client("CW", bot_token=os.environ.get("BOT_TOKEN"), api_id=int(os.environ.get("API_ID")), api_hash=os.environ.get("API_HASH"))
 
